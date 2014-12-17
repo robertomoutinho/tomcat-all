1154d6174f370cbeb5f30d2c0e222194e931e9a8
Roberto Moutinho <roberto.moutinho@match.com>
2014-12-17 10:23:48 -0200

    pushing version to 0.3.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'robertomoutinho@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures tomcat'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.0'
+version          '0.3.1'
 
 depends 'ark', '~> 0.8.2'
 depends 'java', '~> 1.22.0'
