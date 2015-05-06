815091ebae76de67b5a2786b016b29e3eb75a8ac
John Lemp <john@graavy.com>
2015-05-06 09:05:11 -0400

    increase cookbook version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'robertomoutinho@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures tomcat'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.1'
+version          '0.3.2'
 
 depends 'ark', '~> 0.9'
 depends 'java'
