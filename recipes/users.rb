3e0f9f96cbd59008a49e76e939380700001c78e2
John Lemp <john@graavy.com>
2015-05-06 13:24:07 -0400

    set owner and group of tomcat-users.xml to be consistant

--- recipes/users.rb
+++ recipes/users.rb
@@ -1,8 +1,8 @@
 
 template "#{node['tomcat-all']['tomcat_home']}/conf/tomcat-users.xml" do
   source 'tomcat-users.xml.erb'
-  owner 'root'
-  group 'root'
+  owner node['tomcat-all']['user']
+  group node['tomcat-all']['group']
   mode '0644'
   variables(
       :users => TomcatAll::TomcatUsers.tomcat_users(node['tomcat-all']['tomcat_users_data_bag'] , node['tomcat-all']['tomcat_users_search']),
