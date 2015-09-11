1aa80c05c957fd1018a09f5d0f3018b3a29a4937
Ian D. Rossi <ian.rossi@target.com>
2015-09-11 16:29:59 -0400

    Increment version to v0.3.2 reflect changes since v0.3.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'robertomoutinho@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures tomcat'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.1'
+version          '0.3.2'
 
 depends 'ark', '~> 0.9.0'
 depends 'java', '~> 1.31.0'
