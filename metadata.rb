8b163ba7ee8ff21fe672d09e79656f87d72585b9
Sam Dunne <sam@sam-dunne.com>
2015-10-01 15:32:40 +0100

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -8,7 +8,7 @@ version          '0.3.1'
 
 depends 'ark', '~> 0.9.0'
 depends 'java', '~> 1.31.0'
-depends 'apt', '~> 2.4.0'
+depends 'apt', '~> 2.5.0'
 
 supports 'ubuntu', '= 12.04'
 supports 'centos', '>= 6.4'
